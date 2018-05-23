253374387389b1547046098a5c873c881b584365
mvulpe29 <mihaivulpe3@gmail.com>
2018-05-23 15:13:15 +0300

    task filtering by activeUser

--- app/src/main/java/ro/irian/well/well_delivery/repository/TaskListLiveData.java
+++ app/src/main/java/ro/irian/well/well_delivery/repository/TaskListLiveData.java
@@ -3,8 +3,10 @@ package ro.irian.well.well_delivery.repository;
 import android.arch.lifecycle.LiveData;
 import android.util.Log;
 
-import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,30 +16,45 @@ import javax.inject.Singleton;
 
 import ro.irian.well.well_delivery.domain.Task;
 
+//import com.google.android.gms.tasks.Task;
+
 @Singleton
 public class TaskListLiveData extends LiveData<List<Task>> {
 
     public static final String TAG = "TaskListLiveData";
 
-    private CollectionReference collectionReference;
+    private Query documentReference;
 
     private FirebaseFirestore firebaseFirestore;
+
+    private final String MYUSER = "sv23V1ZYF4XZqrMplHG2";
 
     @Inject
     public TaskListLiveData(FirebaseFirestore firebaseFirestore) {
         this.firebaseFirestore = firebaseFirestore;
-        this.collectionReference = firebaseFirestore.collection("tasks");
+        DocumentReference userDocumentReference = firebaseFirestore.collection("users").document(MYUSER);
+        this.documentReference = firebaseFirestore.collection("tasks").whereEqualTo("activeUser", userDocumentReference);
     }
 
     @Override
     protected void onActive() {
         super.onActive();
-        collectionReference.addSnapshotListener((querySnapshot, e) -> {
+        documentReference.addSnapshotListener((querySnapshot, e) -> {
+
             if (e != null) {
                 Log.w(TAG, "Listen failed.", e);
                 return;
             }
 
+
+//            if(task.isSuccessful()){
+//                DocumentSnapshot documentSnapshot = task.getResult();
+//                if(documentSnapshot.exists()){
+//                    List<Task> tasks = (List) documentSnapshot.getData().get("activeTasks");
+//                    Log.d("TAG", "bla");
+//                }
+//            }
+//            List<Task> tasks = task.getResult().
             List<Task> tasks = querySnapshot.getDocuments().stream().map(documentSnapshot -> {
                 Task task = documentSnapshot.toObject(Task.class);
                 task.setId(documentSnapshot.getId());
